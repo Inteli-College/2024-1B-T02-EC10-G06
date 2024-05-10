34b94d1555a29ad5018e937aad29d7344d0772f4
JvLetsBora <99259911+JvLetsBora@users.noreply.github.com>
2024-05-10 19:50:12 -0300

    ADD - Adição da Base do Pyxis Controller

--- src/backend/routers/pyxi.py
+++ src/backend/routers/pyxi.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
-from models.pyxi import PyxiBase, PyxiUpdate  
+from models.pyxi import PyxiBase, PyxiUpdate
+from controller.pyxi import pyxi_created, all_pyxis, one_pyxi, delete_response, update_response, check_pyxi
 from datetime import datetime
 
 from utils.kafka import ProducerController
@@ -15,8 +16,10 @@ server = os.getenv("KAFKA_BROKER")
 client_id = "python-producer"
 apikey = os.getenv("KAFKA_APIKEY")
 password = os.getenv("KAFKA_PASSWORD")
-producer = ProducerController(server,client_id,apikey,password)
 
+producer = ProducerController(server,client_id,apikey,password)
+pyxis_db = client["Hermes"]
+collection = pyxis_db["Pyxis"]
 
 router = APIRouter(
     prefix="/pyxis",
@@ -25,35 +28,35 @@ router = APIRouter(
 
 
 @router.post("/", response_model=PyxiBase)
-def create_pyxi(skip: int = 0, limit: int = 100, producer: ProducerController = Depends(producer)):
-    pyxi = pyxi_created(producer, skip=skip, limit=limit)
+def create_pyxi(pyxi: PyxiBase):
+    pyxi = pyxi_created(producer, pyxi)
     return pyxi
 
 
 @router.get("/", response_model=PyxiBase)
-def get_pyxis(skip: int = 0, limit: int = 100, db: MongoClient = Depends(client)):
-    pyxis = all_pyxis(db, skip=skip, limit=limit)
+def get_pyxis(skip: int = 0, limit: int = 100):
+    pyxis = all_pyxis(collection, skip=skip, limit=limit)
     return pyxis
 
 @router.get("/{pyxi_id}", response_model=PyxiBase)
-def get_pyxi(skip: int = 0, limit: int = 100, db: MongoClient = Depends(client)):
-    pyxi = one_pyxi(db, skip=skip, limit=limit)
+def get_pyxi(skip: int = 0, limit: int = 100):
+    pyxi = one_pyxi(collection, skip=skip, limit=limit)
     return pyxi
 
 
 
 @router.delete("/{pyxi_id}")
-def delete_pyxi(pyxi_id: str, db: MongoClient = Depends(client)):
-    pyxi = get_pyxi(db, pyxi_id=pyxi_id)
+def delete_pyxi(pyxi_id: str):
+    pyxi = check_pyxi(collection, pyxi_id=pyxi_id)
     if pyxi is None:
         raise HTTPException(status_code=404, detail="Pyxis not found")
-    return delete_pyxi(db=db, pyxi_id=pyxi_id)
+    return delete_response(db=collection, pyxi_id=pyxi_id)
 
 
 
 @router.put("/{pyxi_id}", response_model=PyxiUpdate)
-def update_pyxi(pyxi_id: str, pyxi_update: PyxiUpdate, db: MongoClient = Depends(client)):
-    pyxi = get_pyxi(db, pyxi_id=pyxi_id)
+def update_pyxi(pyxi_id: str, pyxi_update: PyxiUpdate):
+    pyxi = check_pyxi(collection, pyxi_id=pyxi_id)
     if pyxi is None:
         raise HTTPException(status_code=404, detail="Pyxis not found")
-    return update_pyxi(db=db, pyxi_id=pyxi_id, pyxi_update=pyxi_update)
+    return update_response(db=collection, pyxi_id=pyxi_id, pyxi_update=pyxi_update)
