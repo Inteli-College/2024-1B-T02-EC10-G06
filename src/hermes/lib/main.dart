afd66fd53fd84b98ba2debba04e40736c1c4e3fc
Kil Matheus <kil.teixeira@sou.inteli.edu.br>
2024-05-21 10:20:21 -0300

    Tela Inicial

--- src/hermes/lib/main.dart
+++ src/hermes/lib/main.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:hermes/admin.dart';
 import 'package:hermes/services/notifi.dart';
-
 void main() {
   NotificationService().initNotification();
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Hermes',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -22,27 +19,64 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
 class MinhaPrimeiraTela extends StatelessWidget {
   const MinhaPrimeiraTela({super.key});
-
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      appBar: AppBar(
-        title: const Text('Minha primeira tela'),
-      ),
-      body: Column(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/hermes.png'), // Substitua 'background_image.jpg' pelo nome do seu arquivo de imagem
+            fit: BoxFit.cover, // Cobrir toda a tela
+          ),
+        ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children:  <Widget>[
+          Spacer(),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: const <Widget>[
+                Text("Hermes",
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+              ],
+            ),
+          ),
           Padding(
             padding: const EdgeInsets.all(8.0),
+           child: Center(
+            child: Text(
+              'Gerenciamento de medicamentos com qualidade dos deuses',
+              style: TextStyle(
+                fontSize: 20.0, // Ajuste o tamanho da fonte conforme necessário
+                fontWeight: FontWeight.w400, // Fonte mais leve
+              ),
+              textAlign: TextAlign.center, // Centraliza o texto dentro do Text widget
+            ),
+           ),
           ),
-          ElevatedButton(onPressed: 
-          (){
-            Navigator.push(context, MaterialPageRoute(builder: (context) => DashboardPage()));
-          }, child: const Text("Mudar de Tela")),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => DashboardPage()),
+                    );
+                  },
+                  child: const Text("Vamos começar"),
+                ),
+              ],
+            ),
+          ),
         ],
       )
+    ),
     );
   }
 }
