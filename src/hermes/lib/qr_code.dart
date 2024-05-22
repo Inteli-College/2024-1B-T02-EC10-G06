2962bb70137837b50e4a2b9bba7bbba1d85430ca
Kil Matheus <kil.teixeira@sou.inteli.edu.br>
2024-05-22 11:34:41 -0300

    Leitor QR_Code

---
+++ src/hermes/lib/qr_code.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+class QRCodePage extends StatefulWidget {
+  const QRCodePage({Key? key}) : super(key: key);
+
+  @override
+  State<QRCodePage> createState() => _QRCodePageState();
+}
+
+class _QRCodePageState extends State<QRCodePage> {
+  String ticket = '';
+  List<String> tickets = [];
+
+  @override
+  void initState() {
+    super.initState();
+    readQRCode(); // Chamando a função para fazer a leitura do QR code ao iniciar a página
+  }
+
+  readQRCode() async {
+    String code = await FlutterBarcodeScanner.scanBarcode(
+      "#FFFFFF",
+      "Cancelar",
+      false,
+      ScanMode.QR,
+    );
+    setState(() => ticket = code != '-1' ? code : 'Não validado');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            if (ticket != '')
+              Padding(
+                padding: const EdgeInsets.only(bottom: 24.0),
+                child: Text(
+                  'Ticket: $ticket',
+                  style: const TextStyle(fontSize: 20),
+                ),
+              ),
+            ElevatedButton.icon(
+              onPressed: readQRCode,
+              icon: const Icon(Icons.qr_code),
+              label: const Text('Validar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
