0a44297a38abc63c1a16dfdd1c373b7bfad94ea6
igor <igor.garcia@sou.inteli.edu.br>
2024-06-07 10:45:35 -0300

    GREATRECEIVER

--- src/hermes/lib/receiver.dart
+++ src/hermes/lib/receiver.dart
@@ -24,67 +24,26 @@ class _ReceiverPageState extends State<ReceiverPage> {
   }
 
   Future<void> _fetchTickets() async {
-    // final response = await http.get(Uri.parse('${dotenv.env["API_URL"]}/tickets'));
+    final response = await http.get(Uri.parse('${dotenv.env["API_URL"]}/tickets'));
 
-    // if (response.statusCode == 200) {
-    //   final List<dynamic> data = jsonDecode(utf8.decode(response.bodyBytes));
-    //   setState(() {
-    //     _tickets = data.map((item) => Ticket.fromJson(item)).toList();
-    //     _openTickets = _tickets.where((ticket) => ticket.status == 'open').toList();
-    //     _isLoading = false;
-    //   });
-    // } else {
-    //   ScaffoldMessenger.of(context).showSnackBar(
-    //     const SnackBar(content: Text('Failed to fetch tickets')),
-    //   );
-      setState(() {
-        _isLoading = false;
-      });
-    //}
-
-    const String mockData = '''
-      [
-        {
-          "idPyxis": "1",
-          "descrition": "Sample ticket 1",
-          "body": ["Item 1", "Item 2"],
-          "created_at": "2022-01-01T00:00:00Z",
-          "fixed_at": "2022-01-01T00:00:00Z",
-          "status": "open",
-          "owner_id": "1",
-          "operator_id": "2"
-        },
-        {
-          "idPyxis": "2",
-          "descrition": "Sample ticket 2",
-          "body": ["Item 1", "Item 2"],
-          "created_at": "2022-01-01T00:00:00Z",
-          "fixed_at": "2022-01-01T00:00:00Z",
-          "status": "closed",
-          "owner_id": "2",
-          "operator_id": "3"
-        },
-        {
-          "idPyxis": "3",
-          "descrition": "Sample ticket 3",
-          "body": ["Item 1", "Item 2"],
-          "created_at": "2022-01-01T00:00:00Z",
-          "fixed_at": "2022-01-01T00:00:00Z",
-          "status": "open",
-          "owner_id": "3",
-          "operator_id": "1"
-        }
-      ]
-      ''';
-
-      final List<dynamic> data = jsonDecode(mockData);
+    if (response.statusCode == 200) {
+      final List<dynamic> data = jsonDecode(utf8.decode(response.bodyBytes));
       setState(() {
         _tickets = data.map((item) => Ticket.fromJson(item)).toList();
         _openTickets = _tickets.where((ticket) => ticket.status == 'open').toList();
+        _isLoading = false;
       });
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Failed to fetch tickets')),
+      );
+      setState(() {
+        _isLoading = false;
+      });
+    }
   }
 
-  void _handleCardTapped(String ticketId) {
+  void _handleCardTapped(String? ticketId) {
     setState(() {
       if (_clickedTicketId == ticketId) {
         _clickedTicketId = null;
@@ -107,85 +66,207 @@ class _ReceiverPageState extends State<ReceiverPage> {
         ],
       ),
       body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : GestureDetector(
-            onTap: () {
-              setState(() {
-                _clickedTicketId = null;
-              });
-            },
-            child: ListView.builder(
-              itemCount: _openTickets.length,
-              itemBuilder: (context, index) {
-                return TicketCard(
-                  ticket: _openTickets[index],
-                  //isExpanded: _openTickets[index].idPyxis == _clickedTicketId,
-                  //onCardTapped: _handleCardTapped,
+      ? Center(child: CircularProgressIndicator())
+      : GestureDetector(
+        onTap: () {
+          setState(() {
+            _clickedTicketId = null;
+          });
+        },
+        child: ListView.builder(
+          itemCount: _openTickets.length,
+          itemBuilder: (context, index) {
+            return TicketCard(
+              ticket: _openTickets[index],
+              isExpanded: _openTickets[index].id == _clickedTicketId,
+              onCardTapped: _handleCardTapped,
             );
-              },
-            ),
-          ),
+          },
+        ),
+      ),
     );
   }
 }
 
-class TicketCard extends StatelessWidget {
+class TicketCard extends StatefulWidget {
   final Ticket ticket;
-  //final bool isExpanded;
-  //final Function(String) onCardTapped;
+  final bool isExpanded;
+  final Function(String?) onCardTapped;
 
   const TicketCard({super.key,
     required this.ticket,
-    //required this.isExpanded,
-    //required this.onCardTapped,
+    required this.isExpanded,
+    required this.onCardTapped,
   });
 
-  // void _toggleFunc() {
-  //   onCardTapped(ticket.idPyxis);
-  // }
+  @override
+  _TicketCardState createState() => _TicketCardState();
+}
+
+class _TicketCardState extends State<TicketCard> {
+  void _toggleFunc() {
+    widget.onCardTapped(widget.ticket.id);
+  }
+
+  Future<void> _confirmClose() async {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Encerrar Ticket'),
+          content: const Text('Deseja encerrar o ticket?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Cancelar'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _closeTicket();
+                Navigator.pop(context);
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+              ),
+              child: const Text('Encerrar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _closeTicket() async {
+    final response = await http.put(
+      Uri.parse('${dotenv.env["API_URL"]}/tickets/${widget.ticket.id}/close'),
+    );
+    if (response.statusCode == 200) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Ticket encerrado com sucesso!'),
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Erro ao encerrar ticket!'),
+        ),
+      );
+    }
+    widget.onCardTapped(widget.ticket.idPyxis);
+  }
+
+  Future<void> _confirmOperate() async {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Operar Ticket'),
+          content: const Text('Deseja operar o ticket?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Cancelar'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _operateTicket();
+                Navigator.pop(context);
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.yellow,
+              ),
+              child: const Text('Operar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _operateTicket() async {
+    final response = await http.put(
+      Uri.parse('${dotenv.env["API_URL"]}/tickets/${widget.ticket.id}/operate'),
+    );
+    if (response.statusCode == 200) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Ticket encaminhado com sucesso!'),
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Erro ao encaminhar o ticket.'),
+        ),
+      );
+    }
+    widget.onCardTapped(widget.ticket.idPyxis);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              ticket.idPyxis,
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
+      child: InkWell(
+        onTap: _toggleFunc,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                widget.ticket.idPyxis,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              ticket.description,
-              style: TextStyle(
-                fontSize: 14,
-                color: Colors.grey[700],
+              const SizedBox(height: 8),
+              Text(
+                widget.ticket.description,
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey[700],
+                ),
               ),
-            ),
-            const SizedBox(height: 8),
-            ...ticket.body.map((medication) => Text('${medication.name}: ${medication.description}')),
-            const SizedBox(height: 8),
-            Text(
-              'Status: ${ticket.status}',
-              style: TextStyle(
-                fontSize: 14,
-                color: Colors.grey[700],
+              const SizedBox(height: 8),
+              ...widget.ticket.body.map((medication) => Text('${medication.name}: ${medication.description}')),
+              const SizedBox(height: 8),
+              Text(
+                'Status: ${widget.ticket.status}',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey[700],
+                ),
               ),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              'Created At: ${ticket.created_at}',
-              style: TextStyle(
-                fontSize: 14,
-                color: Colors.grey[700],
-              ),
-            ),
-          ],
+              const SizedBox(height: 16),
+              widget.isExpanded ? Align(
+                alignment: Alignment.centerRight,
+                child: ElevatedButton(
+                  onPressed: _confirmClose,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                  ),
+                  child: const Text('Encerrar Ticket'),
+                ),
+              ) : Container(),
+              const SizedBox(height: 16),
+              widget.isExpanded ? Align(
+                alignment: Alignment.centerRight,
+                child: ElevatedButton(
+                  onPressed: _confirmOperate,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.yellow,
+                  ),
+                  child: const Text('Operar Ticket'),
+                ),
+              ) : Container(),
+            ],
+          ),
         ),
       ),
     );
